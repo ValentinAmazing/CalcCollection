67a9c4f3f4ede8e73c9f6c7fa1f62bf27e0a4fa9
ValentinAmazing <podrezva@bk.ru>
2024-03-05 23:34:12 +0700

    Initial Commit

---
+++ CalcCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcCollection
+//
+//  Created by Valentin on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
