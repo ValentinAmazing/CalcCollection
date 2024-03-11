0d6aa625d7c0f4c750fe21eb51d572efdb21614d
ValentinAmazing <podrezva@bk.ru>
2024-03-11 08:38:23 +0700

    Добавил вьюКонтроллер. Добавил сториборд, элементы на вью.

---
+++ CalcCollection/ViewControllers/CalcTextFieldValVC.swift
@@ -0,0 +1,24 @@
+//
+//  CalcTextFieldValVC.swift
+//  CalcCollection
+//
+//  Created by Valentin on 11.03.2024.
+//
+
+import UIKit
+
+class CalcTextFieldValVC: UIViewController {
+
+    @IBOutlet weak var operandFirstTF: UITextField!
+    @IBOutlet weak var operandSecondTF: UITextField!
+    @IBOutlet weak var actionTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func resultButton(_ sender: UIButton) {
+    }
+}
